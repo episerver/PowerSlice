00b43dd0d69f80e57cd440da62e9a753e5734c39
Joakim Platbarzdis <joakim@episerver.com>
2014-12-03 16:05:28 +0100

    Bumped version to 2.0.0, redefined dependency range.

--- PowerSlice/Properties/AssemblyInfo.cs
+++ PowerSlice/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PowerSlice")]
-[assembly: AssemblyCompany("Abrahamsson Software AB")]
+[assembly: AssemblyCompany("EPiServer AB")]
 [assembly: AssemblyProduct("PowerSlice")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.*")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
